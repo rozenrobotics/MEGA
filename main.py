f499606d4e3a1269c2600721169a297ee9cefb8f
Ivan Rubtsov <vanischesmall@gmail.com>
2024-08-03 17:41:44 +1000

    add some submodules

---
+++ main.py
@@ -0,0 +1,14 @@
+from submodules.MEGA import MEGA
+import cv2 as cv 
+
+
+if __name__ == "__main__":
+    cap = cv.VideoCapture(0)
+
+    while cv.waitKey(1) != ord('q'):
+        ret, frame = cap.read()
+
+        if not ret:
+            continue
+
+    
